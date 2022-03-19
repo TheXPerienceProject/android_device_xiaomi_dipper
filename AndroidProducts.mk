a9605246b0ec645633b6f5e1318662b2aa5fc167
klozz <carlosj@klozz.dev>
2022-03-19 00:42:53 +0000

    ABANDONED: xperience bringup
    Signed-off-by: klozz <carlosj@klozz.dev>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_dipper.mk
+    $(LOCAL_DIR)/xperience_dipper.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_dipper-user \
-    lineage_dipper-userdebug \
-    lineage_dipper-eng
+    xperience_dipper-user \
+    xperience_dipper-userdebug \
+    xperience_dipper-eng
